873039c4b5c5f0420d7731ed217437d3a1cfddc0
kadmser <kadmser@gmail.com>
2018-04-21 22:26:52 +0300

    refactor

--- src/main/java/ru/sberbank/newsservice/feign/ArticleFeignClient.java
+++ src/main/java/ru/sberbank/newsservice/feign/ArticleFeignClient.java
@@ -1,5 +1,6 @@
 package ru.sberbank.newsservice.feign;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +8,7 @@ import ru.sberbank.newsservice.model.Article;
 
 import java.util.List;
 
+@FeignClient
 public interface ArticleFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/articles/{userId}")
